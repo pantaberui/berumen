2d6df9584276a04ae5644ad64cb97e723126fe9e
Berumen <pantaberumen@gmail.com>
2026-05-06 13:57:23 -0700

    feat: logo del negocio en barra de navegación

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,10 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('admin.dashboard') }}">
+                        <img src="{{ asset('images/logoTexto.png') }}"
+                            alt="Entretenimiento Berumen"
+                            style="height: 45px; width: auto;">
                     </a>
                 </div>
 
